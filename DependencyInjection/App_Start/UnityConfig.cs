71a614b05823aeb9ff675251b05366701957fcaf
BH <BH@DESKTOP-UQTM97C>
2020-03-03 16:44:28 +0600

    new

---
+++ DependencyInjection/App_Start/UnityConfig.cs
@@ -0,0 +1,22 @@
+using System.Web.Mvc;
+using Unity;
+using Unity.Mvc5;
+using BusinessLayer;
+namespace DependencyInjection
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+            
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+            
+            // e.g. container.RegisterType<ITestService, TestService>();
+           container.RegisterType<IEmployee, Employee>();
+            
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
--- DependencyInjection/Controllers/HomeController.cs
+++ DependencyInjection/Controllers/HomeController.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer;
 
 namespace DependencyInjection.Controllers
 {
     public class HomeController : Controller
     {
+        private IEmployee _employee = null;
+
+        public HomeController(IEmployee emoloyee)
+        {
+            _employee = emoloyee;
+
+        }
+
+
         public ActionResult Index()
         {
+            int count = _employee.GetTotalStudent();
             return View();
         }
 
